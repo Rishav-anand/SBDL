04fca619d4119925cb960c5ee682300baaf4ffce
Rishav-anand <anandrishav186@gmail.com>
2025-01-23 14:35:53 +0530

    changing and adding file

--- lib/ConfigLoader.py
+++ lib/ConfigLoader.py
@@ -25,4 +25,4 @@ def get_spark_conf(env):
 
 def get_data_filter(env, data_filter):
     conf = get_config(env)
-    return "true" if conf[data_filter] == "" else conf[data_filter]
+    return True if conf[data_filter] == "" else conf[data_filter]
--- test_pytest_sbdl.py
+++ test_pytest_sbdl.py
@@ -1,5 +1,9 @@
 import pytest
+from datetime import datetime, date
+from pyspark.sql.types import StructType, StructField, StringType, NullType, TimestampType, ArrayType, DateType, Row
 
+from lib import DataLoader
+from lib.ConfigLoader import get_config
 from lib.Utils import get_spark_session
 
 
@@ -8,8 +12,12 @@ def spark():
     return get_spark_session("LOCAL")
 
 
-def test_blank_test(spark):
-    print(spark.version)
-    assert spark.version == "3.5.4"
+def test_get_config():
+    conf_local = get_config("LOCAL")
+    conf_qa = get_config("QA")
+    assert conf_local["kafka.topic"] == "sbdl_kafka_cloud"
+    assert conf_qa["hive.database"] == "sbdl_db_qa"
 
-
+def test_read_accounts(spark):
+    accounts_df = DataLoader.read_accounts(spark, "LOCAL", False, None)
+    assert accounts_df.count() == 8
